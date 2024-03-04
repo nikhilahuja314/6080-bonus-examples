23b3786d50584f00a109c9db37d6896b144748a1
Nikhil Ahuja <nikhilahuja@314@gmail.com>
2024-03-04 22:14:48 +1100

    feat: week 4 event bubbling demo

---
+++ js-event-bubbling/index.js
@@ -0,0 +1,3 @@
+document.getElementById('container-inner').addEventListener('click', () => alert("Inner Container Clicked!"));
+document.getElementById('container-middle').addEventListener('click', () => alert("Middle Container Clicked!"));
+document.getElementById('container-outer').addEventListener('click', () => alert("Outer Container Clicked!"));
